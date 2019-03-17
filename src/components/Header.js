5f10d88f298fb0e9267de2893aa4ac9885c59061
mareenas <persempre1@yandex.ru>
2019-03-18 00:06:20 +0300

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import {BrowserRouter, Link} from "react-router-dom";
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="App row">
+            <ul className="Header mx-auto">
+                <li className="mr-2 mr-sm-4 mr-md-4 mr-lg-5 mr-xl-5">
+                    <Link to="/" >Проекты</Link>
+                </li>
+                <li className="mr-2 mr-sm-4 mr-md-4 mr-lg-5 mr-xl-5">
+                    <Link to="/" >Команда</Link>
+                </li>
+                <li className="mr-2 mr-sm-4 mr-md-4 mr-lg-5  mr-xl-5">
+                    <Link to="/" >БЛОГ</Link>
+                </li>
+                <li className="mr-2 mr-sm-4 mr-md-4 mr-lg-5 mr-xl-5">
+                    <Link to="/" >Контакты</Link>
+                </li>
+            </ul>
+        </div>
+    )
+};
+
+export default Header;
